912de66e50a1810449a23118a1a23e9c26b92b59
ssh30 <ssheogorath@gmail.com>
2020-03-04 02:48:50 +0300

    refactoring

--- Sources/JWT/JWT.swift
+++ Sources/JWT/JWT.swift
@@ -2,30 +2,6 @@ import Foundation
 
 public class JWT {
     
-    public init?(token: String) {
-        let elements = token.split(separator: ".")
-        if elements.count != 3 { return nil }
-        let headerString = String(elements[0])
-        let payloadString = String(elements[1])
-        let secretString = String(elements[2])
-        
-        guard let headerData = Data(base64URLEncoded: headerString),
-            let header = try? JSONSerialization.jsonObject(with: headerData, options: []) as? [String: String],
-            let payloadData = Data(base64URLEncoded: payloadString),
-            let payload = try? JSONSerialization.jsonObject(with: payloadData, options: []) as? [String: String]
-            else { return nil }
-        
-        guard let algString = header["alg"] else { return nil }
-        guard let alg = Algorithm(rawValue: algString) else {
-            print("Alghoritm doesn't support.")
-            return nil
-        }
-        self.alg = alg
-        self.header = header
-        self.payload = payload
-        self.secret = ""
-    }
-    
     public init(alg: JWT.Algorithm = .HS256, secret: String = "") {
         self.alg = alg
         self.header["alg"] = alg.rawValue
@@ -53,14 +29,66 @@ public class JWT {
     public var secret: String
     
     public var token: String? {
-        guard let headerString = try? JSONSerialization.data(withJSONObject: header, options: []).base64URLEncodedString() else { return nil }
-        guard let payloadString = try? JSONSerialization.data(withJSONObject: payload, options: []).base64URLEncodedString() else { return nil }
-        
-        let rawSign = "\(headerString).\(payloadString)"
-        
-        if let sign = Cryptor.hmac(string: rawSign, algorithm: alg.forCryptor, key: secret) {
-            return "\(rawSign).\(sign)"
+        do {
+            let headerString = try JSONSerialization.data(withJSONObject: header, options: []).base64URLEncodedString()
+            let payloadString = try JSONSerialization.data(withJSONObject: payload, options: []).base64URLEncodedString()
+            
+            let rawSign = "\(headerString).\(payloadString)"
+            
+            if let sign = Cryptor.hmac(string: rawSign, algorithm: alg.forCryptor, key: secret) {
+                return "\(rawSign).\(sign)"
+            } else {
+                print("JWT: Can't compute sign.")
+                return nil
+            }
+        } catch let err {
+            print(err.localizedDescription)
+            return nil
         }
-        return nil
+    }
+}
+
+//MARK: - Token decoding
+extension JWT {
+    public convenience init?(token: String) {
+        let elements = token.split(separator: ".").map({String($0)})
+        guard
+            elements.count == 3 else {
+                print("JWT: Wrong format!")
+                return nil
+        }
+        
+        guard let headerData = Data(base64URLEncoded: elements[0]),
+            let payloadData = Data(base64URLEncoded: elements[1]) else {
+                print("JWT: Wrong format!")
+                print("Failed to parse header/payload.")
+                return nil
+        }
+        
+        do {
+            guard let header = try JSONSerialization.jsonObject(with: headerData, options: []) as? [String: String],
+                let payload = try JSONSerialization.jsonObject(with: payloadData, options: []) as? [String: String]
+                else {
+                    print("JWT: Failed to parse header/payload.")
+                    return nil
+            }
+            
+            guard let algString = header["alg"] else {
+                print("JWT: Can't define algorithm.")
+                return nil
+            }
+            guard let alg = Algorithm(rawValue: algString) else {
+                print("JWT: Alghoritm doesn't support.")
+                return nil
+            }
+            
+            self.init(alg: alg, secret: "")
+            self.header = header
+            self.payload = payload
+            
+        } catch let err {
+            print(err.localizedDescription)
+            return nil
+        }
     }
 }
